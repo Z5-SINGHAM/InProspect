c876c4f42791b90e6d2d23095ca8dcaf91b6ebb0
veer1307 <virajlagad@gmail.com>
2019-01-07 16:53:10 +0530

    material-icons added

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import '../node_modules/material-design-icons/iconfont/material-icons.css'
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 /* eslint-disable no-new */
